121dd97c17fd59f4ac23c632c3b546fd16b18f50
Marcin Bojara <marcin_5@mail.ru>
2023-12-02 14:21:04 +0100

    expand footer component

--- src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,5 +1,11 @@
+import copyPasteIcon from "../../assets/copy-paste.svg";
 import s from "./style.module.css";
 
 export function PasswordGeneratorFooter() {
-  return <div className={s.root}>PasswordGeneratorFooter</div>;
+  return (
+    <div className={s.root}>
+      <div>Super password</div>
+      <img className={s.icon} src={copyPasteIcon} alt="Copy paste icon" />
+    </div>
+  );
 }
