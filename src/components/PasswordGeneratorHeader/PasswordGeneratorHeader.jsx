571265e9ecca700a42de06abf795d86cc5db88d2
Marcin Bojara <marcin_5@mail.ru>
2023-12-02 13:30:24 +0100

    expand header component

--- src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
+++ src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
@@ -1,5 +1,11 @@
+import lockSvg from "../../assets/lock.svg";
 import s from "./style.module.css";
 
 export function PasswordGeneratorHeader() {
-  return <div>PasswordGeneratorHeader</div>;
+  return (
+    <div className={s.header}>
+      <div>Password Generator</div>
+      <img src={lockSvg} className={s.icon} alt="Lock icon" />
+    </div>
+  );
 }
