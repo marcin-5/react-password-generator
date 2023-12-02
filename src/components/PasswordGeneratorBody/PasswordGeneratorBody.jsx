498e5bfecb3a1eae035e2428d75d916612be9baf
Marcin Bojara <marcin_5@mail.ru>
2023-12-02 15:14:27 +0100

    handle submit button

--- src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
+++ src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
@@ -1,24 +1,32 @@
 import s from "./style.module.css";
 
-export function PasswordGeneratorBody() {
+export function PasswordGeneratorBody(props) {
+  const submit = (e) => {
+    e.preventDefault();
+    const formData = {};
+    new FormData(e.currentTarget).forEach((value, key) => (formData[key] = value));
+    console.log(formData);
+    props.onSubmit("Generated Password");
+  };
+
   return (
-    <form>
+    <form onSubmit={submit}>
       <div className={s.grid_container}>
         <div className={s.grid_item}>
           Size
-          <input type="number" defaultValue={16} min={6} max={24}/>
+          <input name="size" type="number" defaultValue={16} min={6} max={24} />
         </div>
         <div className={s.grid_item}>
           Uppercase
-          <input type="checkbox" defaultChecked />
+          <input name="uppercase" type="checkbox" defaultChecked />
         </div>
         <div className={s.grid_item}>
           Numbers
-          <input type="checkbox" defaultChecked />
+          <input name="number" type="checkbox" defaultChecked />
         </div>
         <div className={s.grid_item}>
           Specials
-          <input type="checkbox" defaultChecked />
+          <input name="special" type="checkbox" defaultChecked />
         </div>
       </div>
       <div className={s.btn_container}>
--- src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,10 +1,10 @@
 import copyPasteIcon from "../../assets/copy-paste.svg";
 import s from "./style.module.css";
 
-export function PasswordGeneratorFooter() {
+export function PasswordGeneratorFooter(props) {
   return (
     <div className={s.root}>
-      <div>Super password</div>
+      <div>{props.password}</div>
       <img className={s.icon} src={copyPasteIcon} alt="Copy paste icon" />
     </div>
   );
